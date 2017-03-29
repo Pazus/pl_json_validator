a1c6b344a8759fb4f3372a857837fe4f45dd97d9
Pazus <pazzzus@gmail.com>
2017-03-29 09:47:02 +0300

    Update install_utplsql.sh

--- .travis/install_utplsql.sh
+++ .travis/install_utplsql.sh
@@ -8,5 +8,5 @@ git clone https://github.com/utPLSQL/urPLSQL.git --depth 1 utPLSQL
 "$SQLCLI" -L -S sys/$ORACLE_PWD@//$CONNECTION_STR AS SYSDBA <<SQL
 set echo off
 @@utPLSQL/source/install_headless.sql $UT3_USER $UT3_PASSWORD $UT3_OWNER_TABLESPACE
-grant create public synpnym to 
+@@utPLSQL/source/create_synonyms_and_grants_for_public.sql $UT3_USERS
 SQL
