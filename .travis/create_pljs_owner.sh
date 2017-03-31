cdc813662b390679293ee82f342a2f1bcd531f70
Pazus <pazzzus@gmail.com>
2017-03-31 08:10:46 +0300

    fixed user creation

--- .travis/create_pljs_owner.sh
+++ .travis/create_pljs_owner.sh
@@ -5,5 +5,5 @@ cd $(dirname "$(readlink -f "$0")")
 #create user
 "$SQLCLI" -L -S sys/$ORACLE_PWD@//$CONNECTION_STR AS SYSDBA <<SQL
 set echo off
-@@create_pljs_owner.sql $PLJS_USER $PLJS_PASSWORD $PLJS_OWNER_TABLESPACE
+@@create_pljs_owner.sql $PLJS_USER $PLJS_OWNER_PASSWORD $PLJS_OWNER_TABLESPACE
 SQL
--- .travis/create_pljs_owner.sql
+++ .travis/create_pljs_owner.sql
@@ -11,7 +11,4 @@ define ut3_tablespace = &3
 
 @@../source/create_utplsql_owner.sql &&ut3_user &&ut3_password &&ut3_tablespace
 
---only needed to run unit tests for utplsql v3, not required to run utplsql v3 itself
-grant select any dictionary to &ut3_user;
-
 exit success
