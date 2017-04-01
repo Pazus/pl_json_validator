213a70826d9944b2b8465e67e8716941619169a2
Pazus <pazzzus@gmail.com>
2017-04-01 21:31:08 +0300

    fix

--- .travis/create_pljs_owner.sql
+++ .travis/create_pljs_owner.sql
@@ -9,6 +9,6 @@ define ut3_user       = &1
 define ut3_password   = &2
 define ut3_tablespace = &3
 
-@@../source/create_utplsql_owner.sql &&ut3_user &&ut3_password &&ut3_tablespace
+@@../source/create_pljs_owner.sql &&ut3_user &&ut3_password &&ut3_tablespace
 
 exit success
