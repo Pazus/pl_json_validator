322ad8ae9650e8a786f921d38d94e6f303a1826a
Pazus <pazzzus@gmail.com>
2017-04-02 19:59:49 +0300

    q

--- .travis/run_tests.sh
+++ .travis/run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
 
-cd $(dirname "$(readlink -f "$0")")
+#cd $(dirname "$(readlink -f "$0")")
 
 
 #sqlplus /nolog  @utPLSQL/client_source/sqlplus/ut_run.sql $PLJS_OWNER/$PLJS_OWNER_PASSWORD@//$CONNECTION_STR -f=ut_documentation_reporter -s
