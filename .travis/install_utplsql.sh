a61956e0586e2819ed3d017dc06f948a9512c835
Pazus <pazzzus@gmail.com>
2017-04-02 16:00:15 +0300

    asd

--- .travis/install_utplsql.sh
+++ .travis/install_utplsql.sh
@@ -2,11 +2,11 @@
 set -ev
 
 cd $(dirname "$(readlink -f "$0")")
-git clone https://github.com/utPLSQL/utPLSQL.git --branch=master --depth 1 utPLSQL
+git clone https://github.com/utPLSQL/utPLSQL.git --branch=develop --depth 1 utPLSQL
 
 #create user
 "$SQLCLI" -L -S sys/$ORACLE_PWD@//$CONNECTION_STR AS SYSDBA <<SQL
-set echo off
+--set echo off
 @@utPLSQL/source/install_headless.sql $UT3_USER $UT3_PASSWORD $UT3_OWNER_TABLESPACE
 @@utPLSQL/source/create_synonyms_and_grants_for_public.sql $UT3_USERS
 SQL
